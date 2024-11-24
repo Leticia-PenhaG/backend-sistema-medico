44e51a729a51726eaf18309b0d5f47b1b45c43f4
Leticia Peña <leticia.penhag@fpuna.edu.py>
2024-11-24 15:00:06 -0300

    CRUD pacientes

--- sistema-medico-server/server.js
+++ sistema-medico-server/server.js
@@ -28,7 +28,50 @@ const pool = new postgresPool({
 
 pool.connect((err, connection)=>{ 
     if(err) throw err;
-    console.log(`Conexión correcta a sistema_medico database`);
+    console.log(`Conexión exitosa a sistema_medico database`);
 })
 
+app.get("/pacientes",(req, res)=>{
+    const sql = "SELECT * FROM pacientes";
+    pool.query(sql,(err,result)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(result.rows);
+    })
+})
 
+app.get("/pacientes/:id",(req, res)=>{
+    const pacienteId = Number(req.params.id);
+    const sql = "SELECT * FROM pacientes WHERE id=$1";
+    pool.query(sql,[pacienteId],(err,result)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(result.rows[0]);
+    })
+})
+
+app.post("/pacientes",(req, res)=>{
+    const {nombre, apellido, cedula, email, telefono, fechaNacimiento} = req.body;
+    const sql = "INSERT INTO pacientes(nombre, apellido, cedula, email, telefono, fechaNacimiento) VALUES($1,$2,$3,$4,$5,$6) RETURNING *";
+    pool.query(sql,[nombre, apellido, cedula, email, telefono, fechaNacimiento],(err,result)=>{
+        if(err) return res.json(err);
+        return res.status(201).json(result.rows);
+    })
+})
+
+app.patch("/pacientes/:id",(req, res)=>{
+    const pacienteId = Number(req.params.id);
+    const {nombre, apellido, cedula, email, telefono, fechaNacimiento} = req.body;
+    const sql = "UPDATE pacientes SET nombre=$1, apellido=$2, cedula=$3, email=$4, telefono=$5, fechaNacimiento=$6 WHERE id=$6";
+    pool.query(sql,[nombre, apellido, cedula, email, telefono, fechaNacimiento],(err,result)=>{
+        if(err) return res.json(err);
+        return res.status(200).send(`El paciente fue actualizado con éxito para el id:${pacienteId}`);
+    })
+})
+
+app.patch("/Pacientes/:id",(req, res)=>{
+    const pacienteId = Number(req.params.id);
+    const sql = "DELETE FROM pacientes WHERE id=$1";
+    pool.query(sql,[pacienteId],(err,result)=>{
+        if(err) return res.json(err);
+        return res.status(200).send(`El paciente fue eliminado con éxito para el id:${pacienteId}`);
+    })
+})
